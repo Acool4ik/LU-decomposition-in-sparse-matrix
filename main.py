c4e5bc606b45c5341dc3d6d17a639025d654c4c8
Acool4ik <byshkov.oleg@yandex.ru>
2023-12-25 00:14:55 +0300

    add new testcase

--- main.py
+++ main.py
@@ -1,31 +1,36 @@
 import numpy as np
-import scipy.sparse as spsp
+import time
+import scipy.io
 import networkx as nx
+import scipy.sparse as spsp
 import matplotlib.pyplot as plt
 import lu
 import spectral_partitioning
 
+path_to_dataset = "./datasets/494_bus.mtx"
+#path_to_dataset = "./datasets/karate.gml"
 
 if __name__ == '__main__':
     # read data (graph)
-    A = nx.to_scipy_sparse_array(nx.read_gml('karate.gml'))
-    A = A + 5 * spsp.eye(A.shape[0])
+    A = spsp.csr_matrix(scipy.io.mmread(path_to_dataset))
+    #A = nx.to_scipy_sparse_array(nx.read_gml(path_to_dataset))
+    #A = A + 5 * spsp.eye(A.shape[0])
 
     # gets decomposition
+    start = time.time()
     P, L, U = lu.rec_partitioning(A, spectral_partitioning.get_separate_perm)
-    LU = L.dot(U)
+    end = time.time()
+    print("time decomposition [s] = ", end - start)
 
     # checks accuracy
+    LU = L.dot(U)
     print("norm(A) = ", spsp.linalg.norm(A, ord='fro'))
     print("norm(LU) = ", spsp.linalg.norm(LU, ord='fro'))
     print("norm(P(LU)P - A) = ", spsp.linalg.norm(LU[P, :][:, P] - A, ord='fro'))
 
     # shows plot (report)
-    fig, axs = plt.subplots(1, 4)
     data = [A, LU, L, U]
+    fig, axs = plt.subplots(1, len(data))
     for i in range(len(data)):
-        axs[i].spy(data[i], aspect='equal', marker='.', markersize=5)
-        axs[i].grid(True)
-        axs[i].xaxis.set_ticks(np.arange(0, A.shape[0], step=2))
-        axs[i].yaxis.set_ticks(np.arange(0, A.shape[0], step=2))
+        axs[i].spy(data[i], aspect='equal', marker='.', markersize=2, precision=1e-5)
     plt.show()
