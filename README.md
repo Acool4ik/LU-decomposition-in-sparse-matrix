ff5f6dc2b7513ca0d567eb427936f0d1f451ced8
Acool4ik <byshkov.oleg@yandex.ru>
2023-12-25 20:37:46 +0300

    add presentation, little fixes

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 ## lu decomposition in sparse matrix
 
-![fun](https://github.com/Acool4ik/LU-decomposition-in-sparse-matrix/blob/master/images/example1.png)
+### spectral partitioning small dataset
+![fun](https://github.com/Acool4ik/LU-decomposition-in-sparse-
+matrix/blob/master/images/report1.png)
+
+### spectral partitioning medium dataset
+![fun](https://github.com/Acool4ik/LU-decomposition-in-sparse-matrix/blob/master/images/report2.png)
+
+### spectral partitioning big dataset
+![fun](https://github.com/Acool4ik/LU-decomposition-in-sparse-matrix/blob/master/images/report3.png)
+
+### Kernigan-Lin small dataset
+![fun](https://github.com/Acool4ik/LU-decomposition-in-sparse-matrix/blob/master/images/report4.png)
+
+### Kernigan-Lin medium dataset
+![fun](https://github.com/Acool4ik/LU-decomposition-in-sparse-matrix/blob/master/images/report5.png)
+
+### Kernigan-Lin big dataset
+![fun](https://github.com/Acool4ik/LU-decomposition-in-sparse-matrix/blob/master/images/report6.png)
+
+### multi-level recursive bisection see in notebook MGP
+
+### For details see presentation and implementation in code
--- main.py
+++ main.py
@@ -9,9 +9,10 @@ import kernighan_lin
 
 # calc time (f32)     2s            5s              24s
 datasets = ["arc130.mtx", "494_bus.mtx", "dwt_1007.mtx"]
-path_to_dataset = "./datasets/" + datasets[2]
+path_to_dataset = "./datasets/" + datasets[1]
 dtype = np.float32
-fseparate = kernighan_lin.kernighan_lin
+fseparate = kernighan_lin.kernighan_lin  # or spectral_partitioning.get_separate_perm
+fseparate.method_name = "kernighan_lin"
 
 if __name__ == '__main__':
     # read data (graph)
@@ -36,15 +37,17 @@ if __name__ == '__main__':
     lab1 = "A, norm = " + str(np.round(spsp.linalg.norm(A, ord='fro'), 1))
     lab2 = "LU, norm = " + str(np.round(spsp.linalg.norm(LU, ord='fro'), 1))
     lab3 = "P(LU)P, norm = " + str(np.round(spsp.linalg.norm(PLUP, ord='fro'), 1))
+    labels = [lab1, lab2, lab3, "L", "U", "P(LU)P - A"]
     fig, axs = plt.subplots(2, len(data) // 2)
 
     n = A.shape[0]
     subtitle = ("time decomposition [s] = " + str(np.round(end - start, 1)) + ", dataset: "
                 + str(n) + "x" + str(n) + " vertexes, nnz = " + str(A.nnz) +
-                ", method: kernighan_lin")
+                ", method: " + str(fseparate.method_name))
     fig.suptitle(subtitle)
 
     for i in range(2):
         for j in range(len(data) // 2):
             axs[i, j].spy(data[i * 3 + j], aspect='equal', marker='.', markersize=4, precision=1e-5)
+            axs[i, j].set_title(labels[i * 3 + j])
     plt.show()
